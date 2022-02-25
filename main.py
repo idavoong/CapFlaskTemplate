05e554a3911cd46f41f326e358347862683c34dd
Ida Voong <idavoong@gmail.com>
2022-02-25 10:10:52 -0800

    new changes

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 from app import app
 # import os
 
+mongodb+srv://idavoong:tacos@cluster0.sjljj.mongodb.net/capstone?retryWrites=true&w=majority
+
 if __name__ == "__main__":
     
     #os.environ['OAUTHLIB_RELAX_TOKEN_SCOPE'] = '1'
